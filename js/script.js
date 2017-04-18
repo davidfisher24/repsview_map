2f1e60c84c537e5dbc44aed4f09ae631d7cae7d8
Salva <salvador@ngine.co>
2017-04-18 16:59:05 +0200

    new branch  bar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 window.onload = function() {
+  var options = {
+  	mapSize: $('#map').width(),
+  	gpdata: gpdata,
+  	spdata: spdata,
+  }
 
-	var options = {
-		mapWidth: $('#map').width(),
-		mapHeight: $('#map').width(),
-		gpdata: gpdata,
-		spdata: spdata,
-	}
-
-	var mapModel = new MapModel(options);
-	var mapView = new MapView({model: mapModel});
+  var mapModel = new MapModel(options);
+  var mapView = new MapView({model: mapModel});
 }
